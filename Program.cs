5cca66d69850e6fd638fa25413624de33b56f4e1
Davide <bellons91@yahoo.it>
2020-03-07 15:01:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+
+namespace YouTubeVideoDetails
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "<your-api-key-here>",
+            }))
+            {
+                var searchRequest = youtubeService.Videos.List("snippet");
+                searchRequest.Id = "CzvQxQYKO88";
+                var searchResponse = await searchRequest.ExecuteAsync();
+                var youTubeVideo = searchResponse.Items.First();
+               
+                YouTubeVideoDetails videoDetails = new YouTubeVideoDetails()
+                {
+                    VideoId = youTubeVideo.Id,
+                    Description = youTubeVideo.Snippet.Description,
+                    Title = youTubeVideo.Snippet.Title,
+                    ChannelTitle = youTubeVideo.Snippet.ChannelTitle,
+                    PublicationDate = youTubeVideo.Snippet.PublishedAt
+                };
+
+                Console.WriteLine(videoDetails.Description);
+
+            }
+
+        }
+    }
+
+    public class YouTubeVideoDetails
+    {
+        public string VideoId { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string ChannelTitle { get; set; }
+        public DateTime? PublicationDate { get; set; }
+    }
+}
